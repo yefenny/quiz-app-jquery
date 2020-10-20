0bf751de151cdbbce43cc28103fedde59b141752
yefenny <yefennychireno@Yefennys-MacBook-Pro.local>
2020-10-20 16:34:33 -0400

    add pseudo-code

--- app.js
+++ app.js
@@ -6,22 +6,12 @@ const store = {
   questions: [
     {
       question: 'What color is broccoli?',
-      answers: [
-        'red',
-        'orange',
-        'pink',
-        'green'
-      ],
+      answers: ['red', 'orange', 'pink', 'green'],
       correctAnswer: 'green'
     },
     {
       question: 'What is the current year?',
-      answers: [
-        '1970',
-        '2015',
-        '2019',
-        '2005'
-      ],
+      answers: ['1970', '2015', '2019', '2005'],
       correctAnswer: '2019'
     }
   ],
@@ -31,10 +21,10 @@ const store = {
 };
 
 /**
- * 
+ *
  * Technical requirements:
- * 
- * Your app should include a render() function, that regenerates the view each time the store is updated. 
+ *
+ * Your app should include a render() function, that regenerates the view each time the store is updated.
  * See your course material and access support for more details.
  *
  * NO additional HTML elements should be added to the index.html file.
@@ -42,14 +32,36 @@ const store = {
  * You may add attributes (classes, ids, etc) to the existing HTML elements, or link stylesheets or additional scripts if necessary
  *
  * SEE BELOW FOR THE CATEGORIES OF THE TYPES OF FUNCTIONS YOU WILL BE CREATING 👇
- * 
+ *
  */
 
 /********** TEMPLATE GENERATION FUNCTIONS **********/
 
+// This function will run the render functions
+function main() {}
+
 // These functions return HTML templates
+function questionTemplate() {
+  // return html with the question in the title and the answers as radio buttons
+}
+
+function startPageTemplate() {
+  // render a welcome message with a button to start the quiz
+}
+
+function scoreTemplate() {
+  //  calculate the score and return a html with correct format
+}
 
 /********** RENDER FUNCTION(S) **********/
+function render() {
+  // if quizz started render question x
+  // if not  render StartPage
+}
+
+function renderQuestion() {
+  // render question using template
+}
 
 // This function conditionally replaces the contents of the <main> tag based on the state of the store
 
